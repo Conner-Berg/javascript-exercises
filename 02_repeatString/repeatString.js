ab1c47433c937bf10dc7fb8ba19a7c0d820e00e7
Conner-Berg <connerwberg@gmail.com>
2023-01-09 12:51:28 -0500

    fix repeatString.js function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let output = "";
+	for (i = 1; i <= num; ++i) {
+		output += string;
+	}
+	if (num < 0) {
+		output = "ERROR";
+	}
+	return output;
 };
 
 // Do not edit below this line
