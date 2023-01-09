450fd61c8d364f1f5c73b05d1ad02e0bc47f9235
Conner-Berg <connerwberg@gmail.com>
2023-01-09 14:00:23 -0500

    fix reverseString.js function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let output = "";
+	while (string) {
+		output += string.slice(-1);
+		string = string.slice(0, -1);
+	}
+	return output;
 };
 
 // Do not edit below this line
