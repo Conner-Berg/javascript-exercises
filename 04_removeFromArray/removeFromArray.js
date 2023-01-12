a553104d6c95565b87bfb63d8b174eb6bf894250
Conner-Berg <connerwberg@gmail.com>
2023-01-11 23:25:23 -0500

    Fix removeFromArray.js function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...valuesToRemove) {
+	let testArray = [...array];
+	for (let i = array.length - 1; i >= 0; --i) {
+		for (let j = valuesToRemove.length - 1; j >= 0; --j) {
+			if (array[i] === valuesToRemove[j]) {
+				testArray.splice(i, 1);
+			}
+		}
+	}
+	return testArray;
 };
 
 // Do not edit below this line
