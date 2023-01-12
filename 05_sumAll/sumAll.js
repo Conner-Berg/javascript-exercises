df03808fb5d42a882accb8bff12c45ad441b9ac7
Conner-Berg <connerwberg@gmail.com>
2023-01-12 12:55:32 -0500

    Fix sumAll.js function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+	let total = 0;
+	if (Number.isInteger(a) && a >= 0 && Number.isInteger(b) && b >= 0) {
+		if (a <= b) {
+			while (a <= b) {
+				total += a;
+				++a;
+			}
+			return total;
+		} else if (b <= a) {
+			while (b <= a) {
+				total += b;
+				++b;
+			}
+			return total;
+		}
+	} else {
+		return "ERROR";
+	}
 };
 
 // Do not edit below this line
