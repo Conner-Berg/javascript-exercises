342018e770cf65cf7520b3e5440c8aecbdd584a8
Conner-Berg <connerwberg@gmail.com>
2023-01-12 13:57:50 -0500

    Fix both tempConversion.js functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+	let fTempToC = 0;
+	fTempToC = ((fTemp - 32) * 5) / 9;
+	fTempToC = Math.round(fTempToC * 10) / 10;
+	return fTempToC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+	let cTempToF = 0;
+	cTempToF = (cTemp * 9) / 5 + 32;
+	cTempToF = Math.round(cTempToF * 10) / 10;
+	return cTempToF;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
