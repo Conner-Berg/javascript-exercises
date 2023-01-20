842db8b596381fff9a172fe261ee5f72daf9c471
Conner-Berg <connerwberg@gmail.com>
2023-01-19 23:58:51 -0500

    Fix fibonacci.js function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	num = Number(num) - 1;
+	if (num < 0) return "OOPS";
+	let sequence = [1, 1];
+	while (sequence[num] === undefined) {
+		sequence.push(
+			sequence[sequence.length - 1] + sequence[sequence.length - 2]
+		);
+	}
+	return sequence[num];
 };
 
 // Do not edit below this line
