03d080463a04cd011a2f4b0f4bb0469d9ba6c896
Conner-Berg <connerwberg@gmail.com>
2023-01-19 23:57:26 -0500

    Fix palindromes.js function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let strAlpha = "";
+	let strBackwardsAlpha = "";
+	for (let char in str) {
+		if (str[char].toUpperCase() != str[char].toLowerCase()) {
+			// Checks if char is alpha
+			strAlpha = strAlpha + str[char].toLowerCase();
+			strBackwardsAlpha =
+				str[char].toLocaleLowerCase() + strBackwardsAlpha;
+		}
+	}
+	if (strAlpha === strBackwardsAlpha) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
