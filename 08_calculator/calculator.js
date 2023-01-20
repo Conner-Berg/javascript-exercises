97854464f5470a3d105d9ca898a1d2c9093e7261
Conner-Berg <connerwberg@gmail.com>
2023-01-19 23:54:54 -0500

    Fix calculator.js function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,57 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	let total = 0;
+	for (let index in array) {
+		total += array[index];
+	}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	let total = 1;
+	for (let index in array) {
+		total *= array[index];
+	}
+	return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	let total = a;
+	while (b > 1) {
+		total *= a;
+		--b;
+	}
+	if (b === 0) {
+		total = 1;
+	} else if (b <= 0) {
+		total = "ERROR";
+	}
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+	let total = a;
+	while (a > 1) {
+		--a;
+		total *= a;
+	}
+	if (a === 0) total = 1;
+	return total;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
