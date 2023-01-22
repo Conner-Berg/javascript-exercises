e28bba9b4d76cc233964e5c6740136efb9b0ed18
Conner-Berg <connerwberg@gmail.com>
2023-01-22 00:14:30 -0500

    Fix findTheOldest.js function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	// sets yearOfDeath key to current year if missing
+	array.forEach((person) => {
+		if (person.yearOfDeath === undefined) {
+			person.yearOfDeath = new Date().getFullYear();
+		}
+	});
+	// copies people array and adds age key
+	const peopleAges = array.map((person) => ({
+		...person,
+		age: person.yearOfDeath - person.yearOfBirth,
+	}));
+	// finds age key with highest value
+	let highestAge = 0;
+	peopleAges.forEach((person) => {
+		if (person.age > highestAge) {
+			highestAge = person.age;
+		}
+	});
+	// returns object with highest age key
+	return peopleAges.find((person) => {
+		return person.age === highestAge;
+	});
 };
 
 // Do not edit below this line
